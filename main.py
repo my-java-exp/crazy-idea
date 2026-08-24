a9f9d00a5d25efbea7b409ceffcc4dea404d2ce3
my-java-exp <nbk50421@gmail.com>
2026-08-24 16:23:38 +0200

    Add warning about incomplete project
    Added a warning comment indicating that the project is incomplete and should not be run.

--- main.py
+++ main.py
@@ -3,8 +3,4 @@ import src.networks
 import src.game as game
 import src.game_objects as obj
 
-test_input = numpy.random.rand(4, 2)
-
-Game = game.Game(500, 500)
-Game.init_game([obj.BaseObject(10, 10, 0, Game.screen_handler.screen_width, Game.screen_handler.screen_height), obj.StrappedObject(10, 10, 0, Game.screen_handler.screen_width, Game.screen_handler.screen_height, 5, 5, 4), obj.StrappedObject(10, 10, 0, Game.screen_handler.screen_width, Game.screen_handler.screen_height, 5, 5, 4), obj.StrappedObject(10, 10, 0, Game.screen_handler.screen_width, Game.screen_handler.screen_height, 5, 5, 4)])
-Game.run(20)
+# DO NOT RUN ANYTHING BECAUSE THIS PROJECT IS INCOMPLETE
